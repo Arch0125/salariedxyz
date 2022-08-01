3eda2a2548712e59bc8cb545b5e39c0ce86faf31
Arch0125 <dasarchisman25@gmail.com>
2022-08-02 01:23:54 +0530

    Add Account hook

---
+++ src/contracts/hardhat.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
