0cb98935c58394297469fa0d8223377fbd2628c0
Arch0125 <dasarchisman25@gmail.com>
2022-08-26 16:37:38 +0530

    Add Loan Pool UI

--- src/pages/Loan.js
+++ src/pages/Loan.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Borrow from '../components/Borrow';
+import Liquidity from '../components/AddLiquidity';
+import PoolDetails from '../components/PoolDetails';
 
 const Loan = () => {
     return ( 
         <div className='flex flex-col bg-slate-900 h-screen w-screen  items-center justify-center' >
+            <PoolDetails/>
             <div className='flex flex-row w-[90%] h-fit justify-between' >
                 <Borrow/>
-                <div className='flex flex-col w-[40%] bg-white p-6' >
-                </div>
+                <Liquidity/>
             </div>
         </div>
      );
