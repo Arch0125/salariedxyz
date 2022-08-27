0cfd5da61bf3e913d5fc9648dbe76bfead824978
route-2 <ruthurao@gmail.com>
2022-08-27 19:38:50 +0530

    OnboardUI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return ( 
-        <div className='w-screen h-fit p-4 flex flex-row bg-slate-900 items-center justify-center' >
+        <div className='w-screen h-fit p-4 flex flex-row bg-hero items-center justify-center' >
             <label className='text-white font-normal text-lg' ><a target='_blank' href='https://github.com/Arch0125/salariedxyz' >Checkout on GitHub</a></label>
         </div>
      );
--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -11,9 +11,9 @@ const Onboarding = () => {
       <div className="w-full text-white flex-1 items-center justify-center ">
       <div className='flex flex-col flex-wrap  mx-auto sm:w-[40%]  bg-white rounded-xl py-10 p-5 text-slate-900 '>
       <div className="flex flex-row flex-wrap justify-between">
-      <div className="flex flex-col border-0 border-r-2 px-2 items-center ">
+      <div className="flex flex-col border-0 border-r-2 px-10 items-center ">
     <h1 className="text-black font-semibold"> 
-    DAOs Onboarded
+    ORGs Onboarded
     </h1>
     <h1 className="text-black ">
   8735672
@@ -23,7 +23,7 @@ const Onboarding = () => {
 
 
 </div>
-<div className="flex flex-col border-0 border-r-2 px-2">
+<div className="flex flex-col border-0 border-r-2 px-10">
     <h1 className="text-black font-semibold"> 
     Total Value Locked
     </h1>
@@ -35,21 +35,10 @@ $41,885,192.06
 
 
 </div>
-<div className="flex flex-col border-0 border-r-2 px-2">
+
+<div className="flex flex-col px-10">
     <h1 className="text-black font-semibold"> 
-    Total Votes
-    </h1>
-    <h1>
-412
-
-    </h1>
-
-
-
-</div>
-<div className="flex flex-col px-2">
-    <h1 className="text-black font-semibold"> 
-    members
+    Members
     </h1>
     <h1>
 123
@@ -62,7 +51,7 @@ $41,885,192.06
       <div className="flex mt-4  text-white flex-col  ">
 
 <button className="bg-transparent w-1/6 mx-auto hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
-  Launch App
+  Mint NFTs
 </button>
 </div>
       
