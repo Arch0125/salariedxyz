b7028233405124ab4b59a19171ce35296854a2e7
route-2 <ruthurao@gmail.com>
2022-08-27 17:29:07 +0530

    OnboardUI

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Onboarding = () => {
   return (
-    <div className="bg-hero flex flex-col w-screen h-screen items-center justify-between">
+    <div className="bg-hero flex flex-col w-screen h-screen items-center justify-around">
       <div className="h-1/4 mt-80 flex flex-col items-center">
         <label className="text-white font-semibold text-6xl italic ">join the community,</label>
         <label className="text-white font-semibold text-6xl italic ">change the future</label>
       <p className="text-white text-xl mt-6"> Get your salaries  Sala3ied    </p>
       </div>
       <div className="w-full text-white flex-1 items-center justify-center ">
-      <div className='flex flex-col  mx-auto w-[40%] bg-white rounded-xl h-[40%] p-5 text-slate-900 '>
-      <div className="flex flex-row justify-between">
+      <div className='flex flex-col flex-wrap  mx-auto sm:w-[40%]  bg-white rounded-xl py-10 p-5 text-slate-900 '>
+      <div className="flex flex-row flex-wrap justify-between">
       <div className="flex flex-col border-0 border-r-2 px-2 items-center ">
     <h1 className="text-black font-semibold"> 
     DAOs Onboarded
@@ -55,19 +55,21 @@ $41,885,192.06
 123
 
     </h1>
-
-
-
 </div>
-
-
-
+ </div>
+ 
       </div>
-      </div>
+      <div className="flex mt-4  text-white flex-col  ">
+
+<button className="bg-transparent w-1/6 mx-auto hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
+  Launch App
+</button>
+</div>
       
       
-      
       </div>
+     
+
     </div>
   );
 };
