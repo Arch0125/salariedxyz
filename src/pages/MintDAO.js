178309f516b152df5eb38da995082ea923217278
Arch0125 <dasarchisman25@gmail.com>
2022-08-28 10:37:51 +0530

    Update Mint SBT Page

--- src/pages/MintDAO.js
+++ src/pages/MintDAO.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { SBTabi } from './SBTABI';
 import GetContract from '../hooks/GetContract';
 import { ethers } from 'ethers';
+import LoanVaultABI from '../ABIs/LoanVaultABI.json'
 
 const MintDAO = () => {
 
     const[id,setId]=React.useState('');
+    const[daoname,setDaoname]=React.useState('');   
 
-    const SBT = GetContract('0x212B73ca2774A2f271fE4DA4F2F25973ed2DC516', SBTabi);
+    const SBT = GetContract('0x83843047A53edEc47A42e3BaC427FA01390C2c2f', SBTabi);
+    const LoanVault = GetContract('0x212B73ca2774A2f271fE4DA4F2F25973ed2DC516',LoanVaultABI);
 
     const mintsbt = async(id)=>{
         console.log(id)
-        var tx = await SBT.mint('2');
+        var tx = await SBT.mint(id);
+        setName();
         console.log(tx);
+    }
+
+    const setName = async()=>{
+        const name = await LoanVault.registerDAO(daoname);
+        setDaoname(name);
     }
 
     return ( 
@@ -22,7 +31,7 @@ const MintDAO = () => {
                 <br/>
                 <label className='text-2xl text-center' >Unique identity for Everyone</label>
                 <hr/>
-                <input  className='w-full h-fit mt-6 bg-transparent border-2 border-opacity-10 rounded-xl p-3  ' placeholder='Company Name' />
+                <input  className='w-full h-fit mt-6 bg-transparent border-2 border-opacity-10 rounded-xl p-3  ' placeholder='Company Name' onChange={(e)=>setDaoname(e.target.value)} />
                 <input  className='w-full h-fit mt-6 bg-transparent border-2 border-opacity-10 rounded-xl p-3  ' placeholder='Stream Admin' />
                 <input className='w-full h-fit mt-6 bg-transparent border-2 border-opacity-10 rounded-xl p-3  ' placeholder='Add Token ID' onChange={(e)=>setId(e.target.value)} />
                 <button className='w-full h-fit mt-6 bg-white text-slate-900 rounded-xl p-3' onClick={()=>mintsbt(id)} >Mint SBT</button>
