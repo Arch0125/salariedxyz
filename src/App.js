94d861aa568083eec7fec93bb9e9c95d73f77b9f
Archisman Das <dasarchisman25@gmail.com>
2022-12-15 12:53:20 +0530

    Add microsoft clarity

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ import Loan from './pages/Loan';
 import Footer from './components/Footer';
 import Onboarding from './pages/Onboarding';
 import MintDAO from './pages/MintDAO';
+import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
+
 
 function App() {
+
+  useEffect(()=>{
+    if (typeof window !== "undefined") {
+      clarity.init('ey9bk9ce0a')
+    }
+  },[])
+
   return (
     <BrowserRouter>
     <Navbar/>
